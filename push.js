60b0760854ef5fdc782da89c9b46f69d88737ba2
Khushhali Mehta <khushhali.mehta@saeculumsolutions.com>
2020-10-10 12:16:45 +0530

    fix

--- push.js
+++ push.js
@@ -10,13 +10,13 @@ let vepidKyes = {
 
 push.setVapidDetails('mailto:k4khushhali@gmail.com', vepidKyes.publicKey, vepidKyes.privateKey)
 
-// let sub = {
-//     endpoint: "https://fcm.googleapis.com/fcm/send/eNcXddY2EbY:APA91bGuYggNiT_e_2hrUoCe1XNY5Exfz82LEoWP_6Y2jWYMRUV-7uHEa8W1yG_nCdNur6xeQGt4rbUKRtgfzj4NNr93uFOw7wYspXXbdPKJH04YN0UQ24oFrpAu_zp-ASpMERKSJbJc",
-//     expirationTime: null,
-//     keys: {
-//         p256dh: "BAlpIa34O1e45sTTedirMAlWtmK4D639X_iN6z4SK7gv4MzHsO8UuRD2ks3o2nrWlS3WYT3O7myO5bWALkunbkg",
-//         auth: "7Hwc6rxdkO7Ow8fUe6SwIg"
-//     }
-// }
+let sub = {
+    endpoint: "https://fcm.googleapis.com/fcm/send/fwWXstaFHL0:APA91bFRq9171LXCJo33MWtpXpgPBOzUwKU8a1dVGQkYjHHch5QXM9V9ubYHFWa26jUU62y29b_kbN23ix451hk31S0ufH0HmsV26K3Hk-JlmxYWVHV9R6pjp4FY9KF6oFzAK55VtOs3",
+    expirationTime: null,
+    keys: {
+        p256dh: "BCbaAwGzPqvkaYKgjjkNin24gET2OIgKcgG5dtjezrZ_MbUHY_2cX5x1piREWqQxTMb324Ftn3afMp-yOgtzUrQ",
+        auth: "hLyjqdxPxoj9MV1cJKzTaA"
+    }
+}
 
-// push.sendNotification(sub, 'test message')
+push.sendNotification(sub, 'test message')
